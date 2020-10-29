c88ad8fb15a1f9f97d9a551683c40bff9376a39e
zoulei <1815207163@qq.com>
2020-10-29 09:12:41 +0800

    修复日志

---
+++ SourceCode/Framework/Mix.Core/Log/ILogger.cs
@@ -0,0 +1,9 @@
+﻿namespace Mix.Core.Log
+{
+    public interface ILogger
+    {
+        void Error(string message);
+        void Infomation(string message);
+        void Warning(string message);
+    }
+}
---
+++ SourceCode/Framework/Mix.Core/Log/Logger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mix.Core.Log
+{
+    public class Logger : ILogger
+    {
+        public void Infomation(string message)
+        {
+            Serilog.Log.Information(message);
+        }
+
+        public void Warning(string message)
+        {
+            Serilog.Log.Warning(message);
+        }
+
+        public void Error(string message)
+        {
+            Serilog.Log.Error(message);
+        }
+
+        public void Error<T>(string message, T propertyValue)
+        {
+            Serilog.Log.Error(message, propertyValue);
+        }
+    }
+}
